8910a83e796de6ed65b13baa7df878893910488c
George Hotz <geohot@gmail.com>
2021-09-08 13:03:45 -0700

    work

--- contracts/BigInt.sol
+++ contracts/BigInt.sol
@@ -1,4 +1,6 @@
 // https://docs.soliditylang.org/en/v0.8.0/contracts.html
+// https://github.com/kokke/tiny-bignum-c
+
 library BigInt {
   struct bigint {
     uint[] limbs;
@@ -7,6 +9,54 @@ library BigInt {
   function fromUint(uint x) internal pure returns (bigint memory r) {
     r.limbs = new uint[](1);
     r.limbs[0] = x;
+  }
+  
+  function lt(bigint memory _a, bigint memory _b) internal pure returns (bool) {
+    uint limbs = max(_a.limbs.length, _b.limbs.length);
+    for (uint i = limbs-1; i >= 0; --i) {
+      uint a = limb(_a, i);
+      uint b = limb(_b, i);
+      if (a < b) return true;
+      if (a > b) return false;
+    }
+    return false;
+  }
+
+  function mul(bigint memory _a, uint256 b) internal pure returns (bigint memory r) {
+    require(b <= 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF);
+    r.limbs = new uint[](_a.limbs.length+1);
+    uint carry = 0;
+    for (uint i = 0; i < r.limbs.length; ++i) {
+      uint a = limb(_a, i);
+      uint lower = (a & 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF) * b;
+      uint upper = (a >> 128) * b;
+      // TODO: this can overflow
+      r.limbs[i] = lower + (upper << 128) + carry;
+      carry = (upper >> 128);
+    }
+    if (carry > 0) {
+      uint[] memory newLimbs = new uint[](r.limbs.length + 1);
+      uint i;
+      for (i = 0; i < r.limbs.length; ++i)
+        newLimbs[i] = r.limbs[i];
+      newLimbs[i] = carry;
+      r.limbs = newLimbs;
+    }
+  }
+
+  function sub(bigint memory _a, bigint memory _b) internal pure returns (bigint memory r) {
+    r.limbs = new uint[](max(_a.limbs.length, _b.limbs.length));
+    uint borrow = 0;
+    for (uint i = 0; i < r.limbs.length; ++i) {
+      uint a = limb(_a, i);
+      uint b = limb(_b, i);
+      r.limbs[i] = a - b + borrow;
+      if (a - b > a)
+        borrow = 1;
+      else
+        borrow = 0;
+    }
+    // sub can't require a new limb
   }
 
   function add(bigint memory _a, bigint memory _b) internal pure returns (bigint memory r) {
@@ -22,7 +72,6 @@ library BigInt {
         carry = 0;
     }
     if (carry > 0) {
-      // too bad, we have to add a limb
       uint[] memory newLimbs = new uint[](r.limbs.length + 1);
       uint i;
       for (i = 0; i < r.limbs.length; ++i)
